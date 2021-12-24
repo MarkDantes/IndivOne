fb2226181ae8c249dfc5a7189605d680ce702c82
MarkDantes <amirkapkaev@yandex.ru>
2021-12-24 06:49:22 +0300

    Full task

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,63 @@ namespace IndivOne
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.buttonDraw = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonDraw
+            // 
+            this.buttonDraw.Location = new System.Drawing.Point(652, 354);
+            this.buttonDraw.Name = "buttonDraw";
+            this.buttonDraw.Size = new System.Drawing.Size(136, 37);
+            this.buttonDraw.TabIndex = 0;
+            this.buttonDraw.Text = "Draw";
+            this.buttonDraw.UseVisualStyleBackColor = true;
+            this.buttonDraw.Click += new System.EventHandler(this.buttonDraw_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(652, 397);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(136, 37);
+            this.buttonClear.TabIndex = 1;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(634, 435);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDoubleClick);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonDraw);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonDraw;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,158 @@ namespace IndivOne
 {
     public partial class Form1 : Form
     {
+        Graphics g;
+        public SolidBrush b1 = new SolidBrush(Color.Blue);
+        public SolidBrush b2 = new SolidBrush(Color.White);
+        public List<PointF> Points = new List<PointF>();
+        bool dr = false;
+        public Pen pen = new Pen(Color.Gold);
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void buttonDraw_Click(object sender, EventArgs e)
+        {
+            DrawL();
+        }
+        public void DrawL()
+        {
+            dr = true;
+            g = Graphics.FromHwnd(pictureBox1.Handle);
+            if (Jarvis(Points).Count > 1)
+            { 
+                for (int i = 0; i < Jarvis(Points).Count - 1; i++)
+                {
+
+                    g.DrawLine(pen, Jarvis(Points)[i], Jarvis(Points)[i + 1]);
+                   
+
+                }
+                g.DrawLine(pen, Jarvis(Points)[0], Jarvis(Points)[Jarvis(Points).Count - 1]);
+            }
+            g.Dispose();
+            
+        }
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            dr = false;
+            g = Graphics.FromHwnd(pictureBox1.Handle);
+            g.Clear(Color.White);
+            Points.Clear();
+            g.Dispose();
+        }
+
+
+        private static int Orientation(PointF p1, PointF p2, PointF p)
+        {
+            
+            float Orin = (p2.X - p1.X) * (p.Y - p1.Y) - (p.X - p1.X) * (p2.Y - p1.Y);
+
+            if (Orin > 0)
+                return -1;  // Точка P лежит слева
+            if (Orin < 0)
+                return 1; // Точка P лежит справа
+
+            return 0; 
+        }
+        public List<PointF> Jarvis(List<PointF> p)
+        {
+           
+            List<PointF> res = new List<PointF>();
+             var pointOnHull =  p.Where(point => point.X == p.Min(min => min.X)).First();
+
+            PointF endpoint;
+           
+            do
+            {
+                res.Add(pointOnHull);
+                 endpoint = p[0];
+                for (int j = 1; j < p.Count; j++)
+                {
+                    if ((endpoint == pointOnHull) || Orientation(pointOnHull, endpoint, p[j])==-1)
+                    {
+                        endpoint = p[j];
+                    }
+                }
+                if (endpoint != res[0])
+                {
+                    pointOnHull = endpoint;
+                }
+                else
+                    break;
+
+            } while (endpoint != res[0]);
+
+            return res;
+        }
+        
+        public void DrawDots()
+        {
+            g.Clear(Color.White);
+            g = Graphics.FromHwnd(pictureBox1.Handle);
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            for (int i = 0; i < Points.Count; i++)
+                g.FillEllipse(b1, Points[i].X, Points[i].Y, 10, 10);
+
+            g.Dispose();
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            g = (sender as Control).CreateGraphics();
+            PointF p = e.Location;
+            if (e.Button == MouseButtons.Left)
+            {
+                for (int x = 0; x <= 10; x++)
+                {
+                    for (int y = 0; y <= 10; y++)
+                    {
+                        PointF p1 = new PointF();
+                        p1.X = e.X + x;
+                        p1.Y = e.Y + y;
+                        if (Points.Contains(p1))
+                        {
+
+                            g.FillEllipse(b2, p1.X, p1.Y, 10, 10);
+                            Points[Points.IndexOf(p1)] = p;
+                            DrawDots();
+                            if (dr)
+                                DrawL();
+
+                        }
+                        else
+                        {
+                            p1.X = e.X - x;
+                            p1.Y = e.Y - y;
+                            if (Points.Contains(p1))
+                            {
+
+
+                                g.FillEllipse(b2, p1.X, p1.Y, 10, 10);
+                                Points[Points.IndexOf(p1)] = p;
+                                DrawDots();
+                                if (dr)
+                                    DrawL();
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+
+        private void pictureBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            g = (sender as Control).CreateGraphics();
+            PointF p = e.Location;
+
+
+            Points.Add(p);
+                    DrawDots();
+                    if (dr)
+                DrawL();
+               
         }
     }
 }
